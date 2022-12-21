12621ba5942fbf587076a82cfc087886c2d0cdbc
Mahdi Engel <mahdi.engel@gmail.com>
2022-12-21 03:24:23 +0000

    Saved progress on BooleanOperators 2022-12-21 03:24:23 +0000

--- BooleanOperators/src/main/java/Expressions.java
+++ BooleanOperators/src/main/java/Expressions.java
@@ -14,6 +14,7 @@ public class Expressions {
      * @return if b is true, return true. if b is false, return false.
      */
     public boolean returnBoolean(boolean b){
+        if (b == false);
         return false;
     }
 
