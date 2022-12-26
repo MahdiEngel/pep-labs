7a43ce09f6a5f1198d50b2c9e43c181e8c137511
Mahdi Engel <mahdi.engel@gmail.com>
2022-12-26 03:55:44 +0000

    Saved progress on EvenOrOdd 2022-12-26 03:55:44 +0000

--- EvenOrOdd/src/main/java/EvenOrOdd.java
+++ EvenOrOdd/src/main/java/EvenOrOdd.java
@@ -14,6 +14,18 @@ public class EvenOrOdd {
      * @return a String "Even" if n is even, and "Odd" of in is odd. The test cases are case-sensitive.
      */
     public String decide(int n){
-        return "";
+        if (n % 2 == 0) {
+ 
+            // If remainder is zero then this number is even
+            System.out.println("Even");
+        }
+ 
+        else {
+ 
+            // If remainder is not zero then this number is
+            // odd
+            System.out.println("Odd");
+        }
+        return "n";
     }
 }
