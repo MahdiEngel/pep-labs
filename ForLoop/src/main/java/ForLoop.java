4b43b4c42d84388d75c3ead764f8d2df936a9317
Mahdi Engel <mahdi.engel@gmail.com>
2022-12-28 04:38:24 +0000

    Saved progress on ForLoop 2022-12-28 04:38:24 +0000

--- ForLoop/src/main/java/ForLoop.java
+++ ForLoop/src/main/java/ForLoop.java
@@ -40,13 +40,12 @@ public class ForLoop {
      *      @return the sum of all numbers between start and end, with start inclusive and end exclusive.
      */
     public String returnNumbers(int start, int end){
-        String buildingString = Number[];
+        String buildingString = " ";
         
           for (int i = start; i < end; i++){
 
             buildingString = buildingString + i + " ";
-
-        return buildingString;
      }
+          return buildingString;
     }
 }
