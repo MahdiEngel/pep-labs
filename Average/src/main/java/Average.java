5d41eaf0409dfcb73d98b160ce2699c93dd3b48a
Mahdi Engel <mahdi.engel@gmail.com>
2023-01-06 01:53:08 +0000

    Saved progress on Average 2023-01-06 01:53:08 +0000

--- Average/src/main/java/Average.java
+++ Average/src/main/java/Average.java
@@ -18,6 +18,12 @@ public class Average {
      * @return the average of all the values of nums.
      */
     public double getAvg(int[] nums){
-        return 0;
+    	double total = 0;
+    	int lenght = nums.length;
+    	for (int i = 0; i < nums.length; i++){
+    		total = total + nums[i];
+    	}
+    	double avarage = total/nums.length;
+    	return avarage;
     }
 }
