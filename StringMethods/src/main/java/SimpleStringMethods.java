bfb52ee1a6438a26c53e5588f4d7e08cd85d97c4
Mahdi Engel <mahdi.engel@gmail.com>
2023-01-16 19:03:59 +0000

    Saved progress on GroupBy 2023-01-16 19:03:59 +0000

--- StringMethods/src/main/java/SimpleStringMethods.java
+++ StringMethods/src/main/java/SimpleStringMethods.java
@@ -14,7 +14,8 @@ public class SimpleStringMethods {
      * @return the length of string s.
      */
     public int length(String s){
-        return 0;
+       
+        return length(s);
     }
 
     /**
