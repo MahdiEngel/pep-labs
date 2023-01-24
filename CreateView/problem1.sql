116dfbb2e2fed5ea78600a3759f02db8498fe2e7
Mahdi Engel <mahdi.engel@gmail.com>
2023-01-24 17:40:01 +0000

    Saved progress on CreateView 2023-01-24 17:40:01 +0000

--- CreateView/problem1.sql
+++ CreateView/problem1.sql
@@ -1,2 +1,1 @@
---CREATE VIEW firstname_lastname AS SELECT * FROM site_user WHERE firstname, lastname;
-CREATE VIEW firstname_lastname AS SELECT firstname, lastname, FROM site_user;
+CREATE VIEW firstname_lastname AS SELECT firstname, lastname FROM site_user;
